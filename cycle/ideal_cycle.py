9278ef13123ae12843499cf40e048a5729338870
H. Zhu <hz325@tw405.eng.cam.ac.uk>
2019-05-18 13:59:57 +0100

    exp conditions

--- cycle/ideal_cycle.py
+++ cycle/ideal_cycle.py
@@ -5,12 +5,23 @@ import throttle
 import evaporator
 
 
-
-
 "given conditions and initialise nodes"
 fluid = 'R134a'
-p1 = 100000
-p2 = 800000
+p1 = 500000
+p2 = 1600000
+T1 = 293
+T2 = 350
+T3 = 310
+T4 = 300
+Tw_in = 321
+Tw_out = 330
+Ta_in = 293
+Ta_out = 280
+mdot_a = 1.0
+mdot_w = 1.0
+mdot_r = 1.0
+
+
 
 "0-3 Exp nodes, 4-7 Ideal nodes"
 nodes = [Nodes.Node() for i in range(8)]
